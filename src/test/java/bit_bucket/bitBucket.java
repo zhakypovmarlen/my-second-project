681d6f4d6643aa94e11ad237ac601d545180ed38
marlen <marlenzhakypov@gmail.com>
2023-06-09 18:59:29 -0500

    initial commit

---
+++ src/test/java/bit_bucket/bitBucket.java
@@ -0,0 +1,12 @@
+package bit_bucket;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class bitBucket {
+
+    @Test
+    public void asd(){
+        Assert.assertEquals(12, 12);
+    }
+}
