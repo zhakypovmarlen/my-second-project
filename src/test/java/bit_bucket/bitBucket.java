222003fa248035ae551c21e3ea9d551622a07b94
marlen <marlenzhakypov@gmail.com>
2023-06-09 19:02:31 -0500

    changes added in bitBucket class

--- src/test/java/bit_bucket/bitBucket.java
+++ src/test/java/bit_bucket/bitBucket.java
@@ -1,9 +1,19 @@
 package bit_bucket;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class bitBucket {
+
+    @Before
+    public void startup(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
 
     @Test
     public void asd(){
